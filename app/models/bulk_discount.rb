1619196eec1f1159d8f672186bc698fae95fa870
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-15 13:59:20 -0700

    bulk_discount model validation and relationships passing tests

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,3 +1,8 @@
 class BulkDiscount < ApplicationRecord
   belongs_to :merchant
+  has_many :items, through: :merchant
+  has_many :invoice_items, through: :items
+
+  validates :percent_discount, presence: true, numericality: {only_integer: true}
+  validates :threshold, presence: true, numericality: {only_integer: true}
 end

1b31e09ca0b5de718d32f6096895e6d8a1e9f86d
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-15 13:58:52 -0700

    Create bulk_discount model validation and relationship tests

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe BulkDiscount, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to :merchant }
+    it { should have_many(:items).through(:merchant) }
+    it { should have_many(:invoice_items).through(:items) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:percent_discount)}
+    it { should validate_numericality_of(:percent_discount)}
+    it { should validate_presence_of(:threshold)}
+    it { should validate_numericality_of(:threshold)}
+  end
 end

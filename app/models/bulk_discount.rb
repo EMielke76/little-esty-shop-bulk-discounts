e3d0dc47399ecddbf49e7327f8c300fb0693e84e
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-16 11:59:41 -0700

    passing tests

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -5,4 +5,12 @@ class BulkDiscount < ApplicationRecord
 
   validates :percent_discount, presence: true, numericality: {only_integer: true, greater_than: 1, less_than: 100}
   validates :threshold, presence: true, numericality: {only_integer: true, greater_than: 1}
+
+  def format_discount
+    percent_discount.to_s+ "%"
+  end
+
+  def percentage
+    percent_discount/100.0
+  end
 end

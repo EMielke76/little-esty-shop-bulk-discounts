63a396157845f8bdc85946d037b77167048bfb02
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-17 10:37:38 -0700

    Feat: properly acounting for unsuccessful transactions in revenue calculations

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -27,6 +27,10 @@ class Invoice < ApplicationRecord
     merchant_invoice_items(merchant).revenue
   end
 
+  def discounted_revenue_by_merchant(merchant)
+    merchant_invoice_items(merchant).merchant_discounted_revenue
+  end
+
   def invoice_discounted_revenue
     invoice_items.sum do |invoice_item|
       invoice_item.discounted_revenue

1c9f4641ea4bb3fe294ccfee6acf70e88ef94476
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-17 15:04:17 -0700

    Create tests for merchant_discount_creation

---
+++ spec/features/merchants/discounts/new_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "merchant discount creation" do
+
+  it 'displays a from to create a discount' do
+  end
+
+  it 'displays an error when not correctly filled in' do
+  end
+
+  it 'redirects back to discount index upon complettion' do
+  end
+end 

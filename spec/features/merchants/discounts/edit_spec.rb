767ab3a505d832ad475cea72b117f00f39eccd65
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-17 18:55:27 -0700

    Create tests for edit page

---
+++ spec/features/merchants/discounts/edit_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'merchant discount edit page' do
+  it 'displays a form to edit the discount' do 
+  end
+
+  it 'prepopulates the fields with data from the discount' do
+  end
+
+  it 'redirects back to the discount show page and changes are reflected' do
+  end
+end

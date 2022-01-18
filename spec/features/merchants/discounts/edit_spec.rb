0b18a3da200fd38efaf9003b9beffce554680d20
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-17 19:51:45 -0700

    refactor test to reflect the reality on the field

--- spec/features/merchants/discounts/edit_spec.rb
+++ spec/features/merchants/discounts/edit_spec.rb
@@ -37,9 +37,11 @@ RSpec.describe 'merchant discount edit page' do
     within("#update_discount") do
       fill_in(:percent_discount, with: 10)
       fill_in(:threshold, with: 20)
-      click_on "Submit"
+      click_on "Save"
     end
     expect(current_path).to eq("/merchants/#{merchant.id}/discounts/#{bd_1.id}")
+    visit "/merchants/#{merchant.id}/discounts/#{bd_1.id}"
+    
     expect(page).to have_content("Bob Barker's Discount #{bd_1.id}")
     expect(page).to have_content("Percent Discount: 10%")
     expect(page).to have_content("Threshold: 20 items")

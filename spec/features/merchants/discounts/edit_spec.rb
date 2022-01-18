b241d453fbdcdb825dc0dc3ebf1e974bed2db342
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-17 19:15:25 -0700

    Creat test for pre-populated fields in edit form

--- spec/features/merchants/discounts/edit_spec.rb
+++ spec/features/merchants/discounts/edit_spec.rb
@@ -8,12 +8,23 @@ RSpec.describe 'merchant discount edit page' do
 
     visit "/merchants/#{merchant.id}/discounts/#{bd_1.id}/edit"
 
-    expect(page).to have_content("Edit Bob Barker's Discount ##{bd_1.id}")
-    expect(page).to have_field("Percent Discount")
-    expect(page).to have_field("Threshold")
+    within("#update_item") do
+      expect(page).to have_content("Edit Bob Barker's Discount ##{bd_1.id}")
+      expect(page).to have_field("Percent Discount")
+      expect(page).to have_field("Threshold")
+    end
   end
 
   it 'prepopulates the fields with data from the discount' do
+    merchant = create(:merchant, name: "Bob Barker")
+    bd_1 = create(:bulk_discount, merchant: merchant)
+
+    visit "/merchants/#{merchant.id}/discounts/#{bd_1.id}/edit"
+
+    within("#update_item") do
+      expect(page).to have_field(:percent_discount, with: 20)
+      expect(page).to have_field(:threshold, with: 10)
+    end
   end
 
   it 'redirects back to the discount show page and changes are reflected' do

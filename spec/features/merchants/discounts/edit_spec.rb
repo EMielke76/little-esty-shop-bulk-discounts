f5c74e16b9f3881dedab40cef7922e805bc478d1
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-17 20:10:20 -0700

    Create sad-path testing for updating discount

--- spec/features/merchants/discounts/edit_spec.rb
+++ spec/features/merchants/discounts/edit_spec.rb
@@ -41,9 +41,59 @@ RSpec.describe 'merchant discount edit page' do
     end
     expect(current_path).to eq("/merchants/#{merchant.id}/discounts/#{bd_1.id}")
     visit "/merchants/#{merchant.id}/discounts/#{bd_1.id}"
-    
+
     expect(page).to have_content("Bob Barker's Discount #{bd_1.id}")
     expect(page).to have_content("Percent Discount: 10%")
     expect(page).to have_content("Threshold: 20 items")
+  end
+
+  describe 'sad path testing' do
+    it 'displays error messages if incorrect data is entered' do
+      merchant = create(:merchant, name: "Bob Barker")
+      bd_1 = create(:bulk_discount, merchant: merchant)
+
+      visit "/merchants/#{merchant.id}/discounts/#{bd_1.id}/edit"
+
+      within("#update_discount") do
+        fill_in(:percent_discount, with: -10)
+        fill_in(:threshold, with: -10)
+        click_on "Save"
+      end
+      expect(current_path).to eq("/merchants/#{merchant.id}/discounts/#{bd_1.id}/edit")
+
+      within("#errors") do
+        expect(page).to have_content("Error: Percent discount must be greater than 1")
+        expect(page).to have_content("Error: Threshold must be greater than 1")
+      end
+
+      within("#update_discount") do
+        fill_in(:percent_discount, with: 101)
+        fill_in(:threshold, with: 10)
+        click_on "Save"
+      end
+
+      within("#errors") do
+        expect(page).to have_content("Error: Percent discount must be less than 100")
+      end
+    end
+
+    it 'displays error messages if incorrect data type entered' do
+      merchant = create(:merchant, name: "Bob Barker")
+      bd_1 = create(:bulk_discount, merchant: merchant)
+
+      visit "/merchants/#{merchant.id}/discounts/#{bd_1.id}/edit"
+
+      within("#update_discount") do
+        fill_in(:percent_discount, with: "Batman")
+        fill_in(:threshold, with: "Robin")
+        click_on "Save"
+      end
+      expect(current_path).to eq("/merchants/#{merchant.id}/discounts/#{bd_1.id}/edit")
+
+      within("#errors") do
+        expect(page).to have_content("Error: Percent discount is not a number")
+        expect(page).to have_content("Error: Threshold is not a number")
+      end
+    end
   end
 end

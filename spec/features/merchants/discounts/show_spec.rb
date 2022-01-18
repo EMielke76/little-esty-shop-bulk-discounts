4b6961df7bfda52169785257f8a007518d6e634b
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-17 18:41:54 -0700

    Create tests for link to edit form from show

--- spec/features/merchants/discounts/show_spec.rb
+++ spec/features/merchants/discounts/show_spec.rb
@@ -11,6 +11,27 @@ RSpec.describe 'merchant bulk discount show page' do
     expect(page).to have_content("Bob Barker's Discount #{bd_1.id}")
     expect(page).to have_content("Percent Discount: 20%")
     expect(page).to have_content("Threshold: 10 items")
-    save_and_open_page
+  end
+
+  describe 'a link to edit page' do
+    it 'displays a link to edit the discount' do
+      merchant = create(:merchant, name: "Bob Barker")
+      bd_1 = create(:bulk_discount, merchant: merchant)
+
+      visit "/merchants/#{merchant.id}/discounts/#{bd_1.id}"
+
+      exepct(page).to have_content("Edit Discount #{bd_1.id}")
+    end
+
+    it 'links to the edit page' do
+      merchant = create(:merchant, name: "Bob Barker")
+      bd_1 = create(:bulk_discount, merchant: merchant)
+
+      visit "/merchants/#{merchant.id}/discounts/#{bd_1.id}"
+
+      exepct(page).to have_content("Edit Discount #{bd_1.id}")
+      click_on("Edit Discount #{bd_1.id}")
+      expect(current_path).to eq("/merchants/#{merchant.id}/discounts/#{bd_1.id}/edit")
+    end 
   end
 end

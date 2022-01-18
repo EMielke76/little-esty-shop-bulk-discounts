71b487eac9222284450d9496b9e2f5fa194f9676
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-18 16:18:42 -0700

    refactor code to reflect controller name change

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,15 @@ Rails.application.routes.draw do
     resources :invoices, controller: 'admin_invoices'
   end
 
-  get '/merchants/:id/discounts', to: 'merchant_discounts#index'
-  get '/merchants/:merchant_id/discounts/new', to: 'merchant_discounts#new'
-  get '/merchants/:merchant_id/discounts/:discount_id', to: 'merchant_discounts#show'
-  get '/merchants/:merchant_id/discounts/:discount_id/edit', to: 'merchant_discounts#edit'
-  post '/merchants/:merchant_id/discounts', to: 'merchant_discounts#create'
-  patch '/merchants/:merchant_id/discounts/:discount_id', to: 'merchant_discounts#update'
-  delete '/merchants/:merchant_id/discounts/:discount_id', to: 'merchant_discounts#destroy'
+
+
+  get '/merchants/:id/bulk_discounts', to: 'merchant_bulk_discounts#index'
+  get '/merchants/:merchant_id/bulk_discounts/new', to: 'merchant_bulk_discounts#new'
+  get '/merchants/:merchant_id/bulk_discounts/:discount_id', to: 'merchant_bulk_discounts#show'
+  get '/merchants/:merchant_id/bulk_discounts/:discount_id/edit', to: 'merchant__bulk_discounts#edit'
+  post '/merchants/:merchant_id/bulk_discounts', to: 'merchant_bulk_discounts#create'
+  patch '/merchants/:merchant_id/bulk_discounts/:discount_id', to: 'merchant_bulk_discounts#update'
+  delete '/merchants/:merchant_id/bulk_discounts/:discount_id', to: 'merchant_bulk_discounts#destroy'
 
   get '/merchants/:id/invoices', to: 'merchant_invoices#index'
   get '/merchants/:merchant_id/invoices/:invoice_id', to: 'merchant_invoices#show'

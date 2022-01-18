3d5741b1377b8cb3c49ad5c7a41751d1d7c6e99d
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-17 19:00:03 -0700

    Fleshed out first test

--- spec/features/merchants/discounts/edit_spec.rb
+++ spec/features/merchants/discounts/edit_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'merchant discount edit page' do
-  it 'displays a form to edit the discount' do 
+
+  it 'displays a form to edit the discount' do
+    merchant = create(:merchant, name: "Bob Barker")
+    bd_1 = create(:bulk_discount, merchant: merchant)
+
+    visit "/merchants/#{merchant.id}/discounts/#{bd_1.id}/edit"
+
+    expect(page).to have_content("Edit Bob Barker's Discount ##{bd_1.id}")
+    expect(page).to have_field("Percent Discount")
+    expect(page).to have_field("Threshold")
   end
 
   it 'prepopulates the fields with data from the discount' do

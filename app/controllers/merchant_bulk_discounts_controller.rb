2565ca2fb93c35903583f4f87de2f645a19222df
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-18 19:11:33 -0700

    Refactor bulk_discounts_controller to use advanced routing

--- app/controllers/merchant_bulk_discounts_controller.rb
+++ app/controllers/merchant_bulk_discounts_controller.rb
@@ -1,52 +1,53 @@
 class MerchantBulkDiscountsController < ApplicationController
 
   def index
-    @merchant = Merchant.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
   end
 
   def show
     @merchant = Merchant.find(params[:merchant_id])
-    @discount = BulkDiscount.find(params[:discount_id])
+    @discount = BulkDiscount.find(params[:id])
   end
 
   def new
     @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = BulkDiscount.new()
   end
 
   def create
     merchant = Merchant.find(params[:merchant_id])
     discount = BulkDiscount.new(bulk_discount_params)
 
-    if discount.save(bulk_discount_params)
+    if discount.valid?
       merchant.bulk_discounts.push(discount)
-      redirect_to "/merchants/#{merchant.id}/bulk_discounts"
+      redirect_to merchant_bulk_discounts_path(merchant)
     else
       flash[:messages] = discount.errors.full_messages
-      redirect_to "/merchants/#{merchant.id}/bulk_discounts/new"
+      redirect_to new_merchant_bulk_discount_path(merchant)
     end
   end
 
   def edit
     @merchant = Merchant.find(params[:merchant_id])
-    @discount = BulkDiscount.find(params[:discount_id])
+    @discount = BulkDiscount.find(params[:id])
   end
 
   def update
-    discount = BulkDiscount.find(params[:discount_id])
+    discount = BulkDiscount.find(params[:id])
     discount.update(bulk_discount_params)
+
     if discount.valid?
-      redirect_to "/merchants/#{params[:merchant_id]}/bulk_discounts/#{discount.id}"
+      redirect_to merchant_bulk_discount_path(params[:merchant_id], discount)
     else
       flash[:messages] = discount.errors.full_messages
-      redirect_to "/merchants/#{params[:merchant_id]}/bulk_discounts/#{discount.id}/edit"
+      redirect_to edit_merchant_bulk_discount_path(params[:merchant_id], discount)
     end
   end
 
-
   def destroy
-    discount = BulkDiscount.find(params[:discount_id])
+    discount = BulkDiscount.find(params[:id])
     discount.destroy
-    redirect_to "/merchants/#{params[:merchant_id]}/bulk_discounts"
+    redirect_to merchant_bulk_discounts_path(params[:merchant_id])
   end
 
   private

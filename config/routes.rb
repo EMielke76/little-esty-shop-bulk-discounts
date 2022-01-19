c9f237b1e8a618041aa2f5d8c811ba90780274a8
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-18 19:00:11 -0700

    Refactor routes to include advanced routing resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :merchants do
     resources :items, controller: 'merchant_items'
+    resources :bulk_discounts, controller: 'merchant_bulk_discounts'
   end
 
   scope '/admin', as: 'admin' do
@@ -13,13 +14,13 @@ Rails.application.routes.draw do
 
 
 
-  get '/merchants/:id/bulk_discounts', to: 'merchant_bulk_discounts#index'
-  get '/merchants/:merchant_id/bulk_discounts/new', to: 'merchant_bulk_discounts#new'
-  get '/merchants/:merchant_id/bulk_discounts/:discount_id', to: 'merchant_bulk_discounts#show'
-  get '/merchants/:merchant_id/bulk_discounts/:discount_id/edit', to: 'merchant__bulk_discounts#edit'
-  post '/merchants/:merchant_id/bulk_discounts', to: 'merchant_bulk_discounts#create'
-  patch '/merchants/:merchant_id/bulk_discounts/:discount_id', to: 'merchant_bulk_discounts#update'
-  delete '/merchants/:merchant_id/bulk_discounts/:discount_id', to: 'merchant_bulk_discounts#destroy'
+  # get '/merchants/:id/bulk_discounts', to: 'merchant_bulk_discounts#index'
+  # get '/merchants/:merchant_id/bulk_discounts/new', to: 'merchant_bulk_discounts#new'
+  # get '/merchants/:merchant_id/bulk_discounts/:discount_id', to: 'merchant_bulk_discounts#show'
+  # get '/merchants/:merchant_id/bulk_discounts/:discount_id/edit', to: 'merchant__bulk_discounts#edit'
+  # post '/merchants/:merchant_id/bulk_discounts', to: 'merchant_bulk_discounts#create'
+  # patch '/merchants/:merchant_id/bulk_discounts/:discount_id', to: 'merchant_bulk_discounts#update'
+  # delete '/merchants/:merchant_id/bulk_discounts/:discount_id', to: 'merchant_bulk_discounts#destroy'
 
   get '/merchants/:id/invoices', to: 'merchant_invoices#index'
   get '/merchants/:merchant_id/invoices/:invoice_id', to: 'merchant_invoices#show'
